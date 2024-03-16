79c036f7483761d47997fd8b3f3751f1f41ae808
mikeynagler <nagler.may@gmail.com>
2024-03-16 13:43:05 +0200

    feature: modified time indicator on seek

--- src/player/timeline/seek/Seek.tsx
+++ src/player/timeline/seek/Seek.tsx
@@ -1,72 +1,94 @@
-import { useContext, useEffect, useLayoutEffect, useRef } from 'react';
-import './Seek.css';
-import { videoContext } from '../../../context/videoContext';
-import { Draggable } from '../../components/dragable/Draggable';
-import { IDragStatus } from '../../components/dragable/Draggable.types';
+import { useContext, useEffect, useLayoutEffect, useRef } from "react";
+import "./Seek.css";
+import { videoContext } from "../../../context/videoContext";
+import { Draggable } from "../../components/dragable/Draggable";
+import { IDragStatus } from "../../components/dragable/Draggable.types";
+import { formatTime } from "../../../utils/utils";
 
 export const Seek = () => {
-    const { video, isOnTrim, startTime, endTime } = useContext(videoContext);
-    const seekRef = useRef<HTMLDivElement>(null);
-    const touchRef = useRef<HTMLDivElement>(null);
-    const timeRef = useRef<HTMLDivElement>(null);
+  const { video, isOnTrim, startTime, endTime } = useContext(videoContext);
+  const seekRef = useRef<HTMLDivElement>(null);
+  const touchRef = useRef<HTMLDivElement>(null);
+  const timeRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (video?.current) {
-            video.current.addEventListener('timeupdate', handleUpdate)
-        }
-
-        return () => {
-            if (video?.current) {
-                video.current.removeEventListener('timeupdate', handleUpdate)
-            }
-        }
-    }, [video?.current, endTime])
-
-
-
-    const handleUpdate = () => {
-        if (!video?.current || !seekRef.current || !timeRef.current) { return }
-        if(video.current.currentTime > endTime) { return }
-
-        const per = (video.current.currentTime / video.current.duration) * 100;
-        seekRef.current.style.left = per + "%";
-
-       setTime();
+  useEffect(() => {
+    if (video?.current) {
+      video.current.addEventListener("timeupdate", handleUpdate);
     }
 
-    const setTime = () => {
-        if (!video?.current || !timeRef.current) { return }
+    return () => {
+      if (video?.current) {
+        video.current.removeEventListener("timeupdate", handleUpdate);
+      }
+    };
+  }, [video?.current, startTime, endTime]);
 
-        let seconds: number | string = Math.round(video.current.currentTime);
-        let minutes: number | string = Math.round(seconds / 60);
+  useEffect(() => {
+    if (video?.current) {
+      setTime();
+    }
+  }, [video?.current, startTime, endTime]);
 
-        if(seconds < 10) { seconds = '0' + seconds };
-        if(minutes < 10) { minutes = '0' + minutes };
-
-        timeRef.current.innerHTML = `${minutes}:${seconds}`;
+  const handleUpdate = () => {
+    if (!video?.current || !seekRef.current || !timeRef.current) {
+      return;
+    }
+    if (video.current.currentTime > endTime) {
+      return;
     }
 
-    const handleMove = (diff: number, status: IDragStatus) => {
-        if (!seekRef.current || !video?.current) { return }
-        const left = status.startPosition + diff;
-        const parentWidth = seekRef.current.parentElement?.offsetWidth || 0;
+    const per = (video.current.currentTime / video.current.duration) * 100;
+    seekRef.current.style.left = per + "%";
 
-        const per = (left / parentWidth) * 100;
+    setTime();
+  };
 
-        let newTime = (video.current.duration / 100) * per;
-
-        if(newTime < startTime) { newTime = startTime}
-        if(newTime > endTime) { newTime = endTime}
-        video.current.currentTime = newTime;
-
+  const setTime = () => {
+    if (!video?.current || !timeRef.current) {
+      return;
     }
 
-    return (
-        <div className="Seek" ref={seekRef} style={{ visibility: isOnTrim ? 'hidden' :'visible'}}>
-            <Draggable onDrag={handleMove} getTarget={() => seekRef.current} deps={[startTime, endTime]}>
-                <div className="touch" ref={touchRef}></div>
-            </Draggable>
-            <div className='time' ref={timeRef}></div>
-        </div>
-    )
-}
+    const current = Math.floor(video.current.currentTime * 100) / 100;
+    const start = Math.floor(startTime * 100) / 100;
+
+    timeRef.current.innerHTML = formatTime(current - start);
+  };
+
+  const handleMove = (diff: number, status: IDragStatus) => {
+    if (!seekRef.current || !video?.current) {
+      return;
+    }
+    const left = status.startPosition + diff;
+    const parentWidth = seekRef.current.parentElement?.offsetWidth || 0;
+
+    const per = (left / parentWidth) * 100;
+
+    let newTime = (video.current.duration / 100) * per;
+
+    if (newTime < startTime) {
+      newTime = startTime;
+    }
+    if (newTime > endTime) {
+      newTime = endTime;
+    }
+
+    video.current.currentTime = newTime;
+  };
+
+  return (
+    <div
+      className="Seek"
+      ref={seekRef}
+      style={{ visibility: isOnTrim ? "hidden" : "visible" }}
+    >
+      <Draggable
+        onDrag={handleMove}
+        getTarget={() => seekRef.current}
+        deps={[startTime, endTime]}
+      >
+        <div className="touch" ref={touchRef}></div>
+      </Draggable>
+      <div className="time" ref={timeRef}></div>
+    </div>
+  );
+};
